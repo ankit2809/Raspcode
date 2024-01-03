4b8d3da7ae3cd3226baa785651075eb3702e1783
ankit2809 <er.ankit.2809@gmail.com>
2024-01-03 23:35:56 +0100

    Update twitter_chk.py

--- python/twitter_chk.py
+++ python/twitter_chk.py
@@ -4,10 +4,10 @@ import json
 import time
 
 #get your own twitter credentials at dev.twitter.com
-consumer_key = "Z0Tc4aEGSREdjdTIDJrhZpZCz"
-consumer_secret = "W6zq31bATgnPMl24EJwVQcf4s95girUefjAcG8JMfA3LYQ9ldd"
-access_token = "2426267696-GjOXLhAsJzgmw2JZvR12PDAE2dZMhBVLWF7t4Oh"
-access_token_secret = "msTo2T0Z0vsws5BArWqHmGqJ6xpDsk1HZKw8bCRqzHehs"
+consumer_key = "xxxxxx"
+consumer_secret = "xxxxxx"
+access_token = "xxxxxx"
+access_token_secret = "xxxxxx"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
